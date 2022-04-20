e67af8adfd67dfabad197360d7142c115df2d293
Ahmed Sayed <93644109+AhmedS1ayed@users.noreply.github.com>
2022-04-21 00:11:06 +0200

    Add files via upload

--- bashscript.sh
+++ bashscript.sh
@@ -14,6 +14,11 @@ source env/bin/activate
 sudo apt-get install libjpeg-dev zlib1g-dev
 python3 -m pip install pillow
 python3 -m pip install matplotlib
+ python3 -m pip install scikit-build
+pip install --upgrade pip setuptools wheel
 python3 -m pip install opencv-python
-
+python3 -m pip install imageio
+python3 -m pip install moviepy
+python3 -m pip install IPython
+python3 -m pip install getpy
 python3 Lane_line_detection.py $1 $2 $3
